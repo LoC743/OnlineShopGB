9df0d5d7560d561df4f9b730a3995d582eaf88ad
Alexey Morozov <alexey1562@gmail.com>
2021-06-22 13:06:04 +0300

    Добавлена авторизация

--- OnlineShopGB/OnlineShopGB/App/AppDelegate.swift
+++ OnlineShopGB/OnlineShopGB/App/AppDelegate.swift
@@ -9,10 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
+    let requestFactory = RequestFactory()
+    
+    var token: String? // Auth token
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        authRequestFactory()
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = ViewController()
@@ -20,5 +26,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = window
         
         return true
+    }
+    
+    func authRequestFactory() {
+        let auth = requestFactory.makeAuthRequestFatory()
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            switch response.result {
+            case .success(let login):
+                print(login)
+                self.token = login.authToken
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
